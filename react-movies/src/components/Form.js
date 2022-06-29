132c87a7221eb8b4ac13892f4c9200f2e231229e
hamzat <hamzat1.siby@epitech.eu>
2022-06-29 15:41:00 +0200

    connect api

--- react-movies/src/components/Form.js
+++ react-movies/src/components/Form.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Form = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=33a2f6344efc17ed3f1a2c243e2f2081&query=star&language=fr-FR'
+    ).then((res) => setMoviesData(res.data.results));
+
+
+  },[]);
+  
   return (
     <div className="form-component">
       <div className="form-container">
@@ -19,7 +30,9 @@ const Form = () => {
             </div>
         </div>
       </div>
-      <div className="result"></div>
+      <div className="result">
+        {moviesData.map((movie) => )}
+      </div>
     </div>
   );
 };
